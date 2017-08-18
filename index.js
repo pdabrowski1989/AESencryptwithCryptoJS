7fc17d718670faece79f08ed827d0630bdd8de0d
pawel <pawel.dabrowski@unifiedfactory.com>
2017-08-18 14:18:58 +0200

    key size eql 256

--- index.js
+++ index.js
@@ -6,22 +6,20 @@
         var mCtrl = this;
 
         mCtrl.iv = "0000000000000000";
-        mCtrl.key = "04c72343945e2a6ef09221862164ac3a9e914373";
-        mCtrl.plaintext = "abcdefgh12345678";
+        mCtrl.key = "haslo123haslo123";
+        mCtrl.plaintext = "abcdefghijklabcdefghijklabcdefghijklabcdefghijkl";
 
         mCtrl.encryptText = encryptText;
         mCtrl.decryptText = decryptText;
-        mCtrl.encryptTextAESJS = encryptTextAESJS;
 
         /////
 
         function encryptText() {
-            var iv = CryptoJS.enc.Base64.parse(mCtrl.iv);
+            var iv = CryptoJS.enc.Utf8.parse(mCtrl.iv);
             var encrypted = CryptoJS.AES.encrypt(mCtrl.plaintext, mCtrl.key,{
                 iv: iv,
                 mode: CryptoJS.mode.CBC,
-                padding: CryptoJS.pad.ZeroPadding,
-                keySize: 128
+                keySize: 256
             });
 
             mCtrl.output = encrypted.toString()
@@ -38,12 +36,6 @@
             });
 
             mCtrl.decrypted = decrypted.toString(CryptoJS.enc.Utf8)
-        }
-
-        function encryptTextAESJS() {
-            console.log(CryptoJS.enc.Utf8.parse(mCtrl.key).toString(), CryptoJS.enc.Utf8.parse(mCtrl.iv).toString())
-
-          var aesCbc = new aesjs.ModeOfOperation.cbc([], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
         }
     }
 })();
